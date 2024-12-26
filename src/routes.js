7b04b6bffbff2ca9bad31c07c7e138d02820cde9
Joe Warren <joe.warren7@protonmail.com>
2024-12-26 00:10:37 -0600

    feat: add routing

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
 import { Routes, Route } from 'react-router';
 import HomesFeed from './components/homes-feed/homes-feed';
-// import Home from './components/home';
+// import HomeCard from './components/home-card/home-card';
 import Error from './components/error/error';
 export default (
   <Routes>
     <Route path='/' element={<HomesFeed />}></Route>
-    {/* <Route path='/users/:username' element={<Home />}></Route> */}
+    {/* <Route path='/homes/:id' element={<HomeCard />}></Route> */}
     <Route path='*' element={<Error />}></Route>
   </Routes>
 );
