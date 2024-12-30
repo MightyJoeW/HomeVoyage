10ce42baef3ffbebfa6a4b9157455dd9fb5317d0
Joe Warren <joe.warren7@protonmail.com>
2024-12-30 14:59:09 -0600

    fix: align photos in mobile view

--- src/components/home-card/home-card.tsx
+++ src/components/home-card/home-card.tsx
@@ -1,5 +1,9 @@
 import { FC } from 'react';
 import { Link, useLocation } from 'react-router';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
 import { HomeApiResponse } from '../../types/homes-types';
 
 interface PhotoProps {
@@ -21,35 +25,67 @@ const enabledLink: PhotoProps = {
 
 const HomeCard: FC<{ photo: HomeApiResponse }> = ({ photo }) => {
   const location = useLocation();
+  const isLinkEnabled = location.pathname === '/' ? enabledLink : disabledLink;
+
   return (
-    <div style={{ textAlign: 'center', margin: '10px' }}>
-      <Link to={`homes/${photo?.id}`}>
-        <img
-          className='img'
-          alt='photo.description'
-          src={
-            location?.pathname === '/'
-              ? photo?.urls?.small
-              : photo?.urls?.regular
-          }
-          style={location.pathname === '/' ? enabledLink : disabledLink}
+    <Card sx={{ maxWidth: 345, textAlign: 'center', marginBottom: '4px' }}>
+      <Link to={`homes/${photo?.id}`} style={isLinkEnabled}>
+        <CardMedia
+          component='img'
+          image={photo.urls.regular}
+          alt={`${photo.alt_description}`}
         />
       </Link>
-      <a
-        className='credit'
-        target='_blank'
-        rel='noreferrer noopener'
-        href={`https://unsplash.com/@${photo?.user?.username}`}
-        style={{
-          display: 'block',
-          marginTop: '8px',
-          textDecoration: 'none',
-          color: '#000',
-        }}
-      >
-        <strong>Photographer:</strong> {photo?.user?.name}
-      </a>
-    </div>
+      <CardContent>
+        <Typography variant='body2' color='text.secondary'>
+          123 Main St
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          {'Allen, TX 75013'}
+        </Typography>
+        <Typography variant='body2' color='text.secondary'>
+          3 Beds | 2 Baths | 1,200 Sq Ft | 66 Days on Market
+        </Typography>
+      </CardContent>
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant='h5'
+          component='div'
+          color='text.secondary'
+        >
+          {`For Sale ${'$474,900'}`}
+        </Typography>
+        {location.pathname === '/' && (
+          <Typography gutterBottom variant='body1' component='div'>
+            <Link
+              style={{
+                ...isLinkEnabled,
+                textDecoration: 'none',
+                color: '#0000EE',
+              }}
+              to={`homes/${photo?.id}`}
+            >
+              {'Show details >'}
+            </Link>
+          </Typography>
+        )}
+        <Typography variant='subtitle2' color='text.secondary'>
+          Photo courtesy of{' '}
+          <a
+            target='_blank'
+            rel='noreferrer noopener'
+            href={`https://unsplash.com/@${photo?.user?.username}`}
+            style={{
+              textDecoration: 'none',
+              color: '#00000099',
+            }}
+          >
+            {photo.user.name}
+          </a>
+        </Typography>
+      </CardContent>
+    </Card>
   );
 };
 
